7132f1ad34f8549fd5370758e79b105c58bd79b6
IIKUYY <elierley54@gmail.com>
2023-07-28 21:28:50 -0600

    Docs: Agregar resumen del capitulo 7
    ## what
    
    - Agregar el resumen del capitulo 7 junto con sus ejemplos
    
    ## why
    
    - Continuar con las tareas dadas
    
    ## references
    - http://diveintohtml5.info/index.html

---
+++ Ch7/Ejemplo/README.md
@@ -0,0 +1,3 @@
+# [HTML](https://github.com/IIKUYY/HTML5/blob/main/Ch7/Ejemplo/index.html)
+
+# [JavaScript](https://github.com/IIKUYY/HTML5/blob/main/Ch7/Ejemplo/main.js)
---
+++ Ch7/Ejemplo/main.js
@@ -0,0 +1,17 @@
+var counterVal = parseInt(localStorage.getItem('counterValue')) || 0;
+updateDisplay(counterVal);
+
+function incrementClick() {
+  updateDisplay(++counterVal);
+  localStorage.setItem('counterValue', counterVal);
+}
+
+function resetCounter() {
+  counterVal = 0;
+  updateDisplay(counterVal);
+  localStorage.setItem('counterValue', counterVal);
+}
+
+function updateDisplay(val) {
+  document.getElementById("label").innerHTML = val;
+}
