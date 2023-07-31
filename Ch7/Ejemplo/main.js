1e1780a2413858d51040632af48daf3e49974248
IIKUYY <elierley54@gmail.com>
2023-07-31 16:14:40 -0600

    Chores: Realizar las tareas dadas
    ## what
    
    - Mover las funciones al principio del código
    - Mover el <script> adentro de la etiqueta body
    
    ## why
    
    - Arreglar la semántica de los archivos
    - Mejorar la optimización del código
    
    ## references
    -

--- Ch7/Ejemplo/main.js
+++ Ch7/Ejemplo/main.js
@@ -1,17 +1,17 @@
-var counterVal = parseInt(localStorage.getItem('counterValue')) || 0;
-updateDisplay(counterVal);
-
 function incrementClick() {
   updateDisplay(++counterVal);
-  localStorage.setItem('counterValue', counterVal);
+  localStorage.setItem("counterValue", counterVal);
 }
 
 function resetCounter() {
   counterVal = 0;
   updateDisplay(counterVal);
-  localStorage.setItem('counterValue', counterVal);
+  localStorage.setItem("counterValue", counterVal);
 }
 
 function updateDisplay(val) {
   document.getElementById("label").innerHTML = val;
 }
+
+var counterVal = parseInt(localStorage.getItem("counterValue")) || 0;
+updateDisplay(counterVal);
